4b6607af4a3f8a396559f957a9ae10a13e048b15
gabriel <gabriel.ratye@etudiant-eemi.com>
2013-11-10 19:06:08 +0100

    ajustements sur le menu

--- ressources/js/function.js
+++ ressources/js/function.js
@@ -6,11 +6,17 @@ $(document).ready(function () {
 	$(document).scroll(function(){
 		if($(this).scrollTop() > withoutbg) 
 		{
-			$('header').css({"background":"none"});
+			$('header').css({"background":"none","border-bottom":"none"});
+			$('header nav div , header nav ul li').css({"border-left":"none","border-bottom":"none","border-right":"none"});
+			$('#search-bar input').css({"border":"1px solid #333","background":"none"})
 		}
 		if($(this).scrollTop() > withbg - 50) 
 		{
-			$('header').css({"background":"#333333"});
+			$('header').css({"background":"#333333","border-bottom":"#454545 solid 1px"});
+			$('header nav div, header nav ul li').css({"border-bottom":"1px solid #252525","border-left":"#424242 solid 1px","border-right":"#252525 solid 1px"});
+			$('header nav div').first().css({"border-bottom":"none","border-left":"none","border-right":"#252525 solid 1px"});
+			$('header nav div').last().css({"border-bottom":"none","border-left":"#424242 1px solid","border-right":"none"});
+			$('#search-bar input').css({"border":"none","background":"#252525"})
 		}
 		
 	});
