9a6c42697fd070eb7e7a44a216eb3e24ca7f1823
Quentin <quentin.lemoine12@gmail.com>
2013-11-13 16:53:29 +0100

    page projet intégrer, a tester sur autre browser

--- ressources/js/function.js
+++ ressources/js/function.js
@@ -28,12 +28,41 @@ $(document).ready(function () {
 	$('.perks').on('click',function(){
 		$("input").prop("checked",false);
 
-
 		$("input",this).prop("checked",true);
-		
-
 	});
 	
+
+	/**** Video mode cinema ***/
+	var videoproj = $('.video-proj').offset().top;
+	var infoproj = $('.info-proj').offset().top;
+
+	$('.need-proj video, .play').on('click',function(){
+		
+		$(document).scrollTo(infoproj,500);
+		$('.cine-mode').fadeIn(500);
+		$('.play').fadeOut(500);
+		$(".video-proj").animate({
+			"height":"600px"			
+
+		},{
+			queue:true,
+			duration:500
+		});
+	})
+
+	$('.hide-cine-mode').on('click',function(){
+		$('.cine-mode').fadeOut(1500);
+		$('.play').fadeIn(1000);
+		$('.video-proj').animate({
+			"height":"300px"
+			
+		},{
+			queue:true,
+			duration:1000
+		});
+		
+		$(document).scrollTo(infoproj,1000);
+	})
 
 	/***HEADER BACKGROUND***/
 	/*var withbg = $('.proj-featured').offset().top;
