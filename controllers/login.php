3b5e66231291050932929b260eadd0b097ec6d79
Thebigbignooby <whishana@gmail.com>
2013-11-27 14:27:53 +0100

    test reformattage controlleur/login.php

--- controllers/login.php
+++ controllers/login.php
@@ -1,50 +1,44 @@
 <?php
-	if(isset($_POST["nomco"]))
-		{			
-			$veriflog = new UserModel;
-			
-			$infocompte = $veriflog->verif_log($_POST['nomco'], $_POST['mdpco']);
-			
-			if ($infocompte) 
-			{
-			/*
 
-				if($infocompte->USER_ADMIN == 0)
-				{
-					header("location:index");
-				}
-				
-					
-				else
-				{
-											
-					if ($_POST["remember"]=="checked")
-						{
-							setcookie("nom",$_POST['nomco'],time()+3600*24*31);
-							setcookie("mdp",md5($_POST['mdpco']),time()+3600*24*31);
-							
-						}
-					*/
+if (isset($_POST["nomco"])) {
+    $veriflog = new UserModel;
 
-							session_start();
-							
-								$_SESSION["user_name"] = $infocompte->user_name;
-								$_SESSION["user_mail"] = $infocompte->user_mail;
-								$_SESSION["user_password"] = $infocompte->user_password;
-								$_SESSION["user_freelance"] = $infocompte->user_freelance;	
-						
-					
+    $infocompte = $veriflog->verif_log($_POST['nomco'], $_POST['mdpco']);
 
-							
-							header("location:project");
-					
-				}
+    if ($infocompte) {
+        /*
 
-			else
-			{
-				header("location:don");
-			}
-	}
-	?>
+          if($infocompte->USER_ADMIN == 0)
+          {
+          header("location:index");
+          }
+
+
+          else
+          {
+
+          if ($_POST["remember"]=="checked")
+          {
+          setcookie("nom",$_POST['nomco'],time()+3600*24*31);
+          setcookie("mdp",md5($_POST['mdpco']),time()+3600*24*31);
+
+          }
+         */
+
+        session_start();
+
+        $_SESSION["user_name"] = $infocompte->user_name;
+        $_SESSION["user_mail"] = $infocompte->user_mail;
+        $_SESSION["user_password"] = $infocompte->user_password;
+        $_SESSION["user_freelance"] = $infocompte->user_freelance;
+
+
+
+
+        header("location:project");
+    } else {
+        header("location:don");
+    }
+}
+?>
 					
-	
