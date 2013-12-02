d54d15556f9571540436aa26c0471dd8d4c68599
Rory Dent <whishana@gmail.com>
2013-12-02 14:51:12 +0100

    config serveur

--- libs/Config.php
+++ libs/Config.php
@@ -1,9 +1,9 @@
 <?php
 
-/*define('DB_HOST', 'mysql51-103.perso');
-define('DB_NAME', 'crawd');
-define('DB_USER', 'root');
-define('DB_PASS', '');*/
+//define('DB_HOST', 'mysql51-103.perso');
+//define('DB_NAME', 'crawd');
+//define('DB_USER', 'crawd');
+//define('DB_PASS', 'eemi2013');
 
 
 define('DB_HOST', 'localhost');
